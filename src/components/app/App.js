fd2702fd37533ad89ec5b526697923a9e397e366
Maiiialen <magdalena.salega@gmail.com>
2023-02-03 16:29:23 +0100

    feat: Add routing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,26 @@
+// import { useState } from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "../home/Home"
+import Store from "../store/Store"
+import Details from "../details/Details"
+import Cart from "../cart/Cart"
+import Navbar from "../navbar/Navbar"
 import './App.scss';
 
 function App() {
-  return (
-    <div className="app">
-    </div>
-  );
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/store" element={<Store />} />
+                    <Route path="/store/:id" element={<Details />} />
+                    <Route path="/cart" element={<Cart />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
